79fc895bb9ee8091fd0330b8c19fc26dd4dc9796
Carlos Martinez <carlos@lacnic.net>
2013-09-10 01:34:33 -0300

    commit

--- bin/rpki-stats.py
+++ bin/rpki-stats.py
@@ -47,9 +47,9 @@ class CLIError(Exception):
         return self.msg
     def __unicode__(self):
         return self.msg
-
-def main(argv=None): # IGNORE:C0111
-    '''Command line options.'''
+    
+#------------------------------------------------------------------------------------
+def stats_runner(argv=None):
     
     if argv is None:
         argv = sys.argv
@@ -73,129 +73,140 @@ def main(argv=None): # IGNORE:C0111
   or conditions of any kind, either express or implied.
 
 USAGE
-''' % (program_shortdesc, str(__date__))
+''' % (program_shortdesc, str(__date__))    
 
     program_banner = '''
         (c) Carlos M. Martinez, carlos@lacnic.net, 2013-09-05
         version %s\n
-    ''' % (program_version_message)
-
-    try:
-        print "*** RPKI Stats Tooling\n"
-        print program_banner
-        # Setup argument parser
-        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
-        # parser.add_argument("-r", "--rir", dest="rir", help="RIR Name to process. [default: %(default)s]", metavar="RIR" )
-        parser.add_argument("-q", "--file-query", dest="filequery", help="Python-like query to be read from file and run via eval(). [default: %(default)s]", metavar="QUERY" )
-        parser.add_argument('-V', '--version', action='version', version=program_version_message)
-        parser.add_argument(dest="query", help="paths to folder(s) with source file(s) [default: %(default)s]", metavar="query", nargs='?')
+    ''' % (program_version)
+    
+    print "*** RPKI Stats Tooling\n"
+    print program_banner
+    # Setup argument parser
+    parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+    # parser.add_argument("-r", "--rir", dest="rir", help="RIR Name to process. [default: %(default)s]", metavar="RIR" )
+    parser.add_argument("-q", "--file-query", dest="filequery", help="Python-like query to be read from file and run via eval(). [default: %(default)s]", metavar="QUERY" )
+    parser.add_argument('-V', '--version', action='version', version=program_version_message)
+    parser.add_argument(dest="query", help="paths to folder(s) with source file(s) [default: %(default)s]", metavar="query", nargs='?')
+    
+    # Process arguments
+    args = parser.parse_args()
+    
+    # rir = args.rir
+    rir = 'lacnic'
+    query = None
+    filequery = None
+    if args.query:
+        query = " ".join(args.query)
+    else:
+        query = None
         
-        # Process arguments
-        args = parser.parse_args()
-        
-        # rir = args.rir
-        rir = 'lacnic'
-        if args.query:
-            query = " ".join(args.query)
-        else:
-            query = None
+    ## if queryfile specified, we run the file and check for the expected functions and values
+    if args.filequery:
+        filequery = args.filequery
+        fq_locals = {}
+        execfile(filequery, globals(), fq_locals )
+        try:
+            rq_class = fq_locals['RoaQuery']
+            roa_query_in = rq_class()
+        except:
+            raise
+        #===================================================================
+        # if not type(myloc.get('roa_test', None)):
+        #     print myloc, myloc.get('roa_test')
+        #     raise CLIError("Function roa_test not found in file %s" % (queryfile))
+        # if not myloc.get('roa_match', None):
+        #     myloc['roa_match'] = lambda x,y:True
+        # if type(myloc.get('roa_no_match') != type(lambda x:x)):
+        #     myloc['roa_no_match'] = lambda x,y:True
+        #===================================================================
             
-        ## if queryfile specified, we run the file and check for the expected functions and values
-        if args.filequery:
-            filequery = args.filequery
-            fq_locals = {}
-            execfile(filequery, globals(), fq_locals )
-            try:
-                rq_class = fq_locals['RoaQuery']
-                roa_query_in = rq_class()
-            except:
-                raise
-            #===================================================================
-            # if not type(myloc.get('roa_test', None)):
-            #     print myloc, myloc.get('roa_test')
-            #     raise CLIError("Function roa_test not found in file %s" % (queryfile))
-            # if not myloc.get('roa_match', None):
-            #     myloc['roa_match'] = lambda x,y:True
-            # if type(myloc.get('roa_no_match') != type(lambda x:x)):
-            #     myloc['roa_no_match'] = lambda x,y:True
-            #===================================================================
-                
-        if not query and not filequery:
-            sys.stderr.write("Either query or filequery MUST be defined.\n")
-            raise CLIError("Either query or filequery MUST be defined.\n")
-        
-        ## init logging
-        dp = commons.dprint.dprint()
-        
-        # init local stats
-        sk = commons.statkeeper.statkeeper()
-        
-        # get delegated
-        dp.log("Downloading stat file for RIR %s..." % (rir))
-        dlg_tmpfile = commons.utils.get_tmp_file_name("delegated-lacnic-latest")
-        commons.getfile.getfile("ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest", dlg_tmpfile, 43200)
-        dp.log(" OK\n")
-        
-        dp.log("Importing delegated stats in memory... ")
-        dlg_api = delegated.api.Delegated(dlg_tmpfile)
-        dlg_api.read_delegated()
-        dp.log(" OK\n")        
-        
-        dp.log("Dowloading validator output... ")
-        dlg_tmpfile = commons.utils.get_tmp_file_name("lacnic-roa-prefixes.csv")
-        commons.getfile.getfile("http://ripeval.labs.lacnic.net/rpki/batch-validation/latest/lacnic.tal-roa-prefixes.csv", dlg_tmpfile, 3600)        
-        dp.log(" OK\n")
-        
-        dp.log("Importing RIPE validator output for RIR %s... " % (rir))
-        ripeval_stats = rpki.ripeval_stats.BatchValidationResults()
-        ripeval_stats.read_csv(dlg_tmpfile)
-        dp.log(" OK\n")
-        
-        ## process ROAs
-        rx = ripeval_stats.query("1=1", {})
-        for row in rx:
-            sk.incKey('ripeval-proc-rows')
-            pfx1 = ipaddr.IPNetwork(row['prefix'])
-            pfx2 = ipaddr.IPNetwork(pfx1.network)
-            drec = dlg_api.resource_find_inside(str(pfx2))
-            if drec:
-                if roa_query_in:
-                    if roa_query_in.roa_test(drec, row):
-                        sk.incKey('matched-rows')
-                        roa_query_in.roa_match(drec, row)
-                    else:
-                        sk.incKey('non-matched-rows')
-                        roa_query_in.roa_no_match(drec, row)
-                elif query:
-                    if eval(query, None, drec):
-                        print "prefix %s, alloc_pfx %s, origin_as %s, cc %s" % (row['prefix'], drec['prefix'], row['origin_as'], drec['cc'])
-                        sk.incKey('matched-rows')
+    if not query and not filequery:
+        sys.stderr.write("Either query or filequery MUST be defined.\n")
+        raise CLIError("Either query or filequery MUST be defined.\n")
+    
+    ## init logging
+    dp = commons.dprint.dprint()
+    
+    # init local stats
+    sk = commons.statkeeper.statkeeper()
+    
+    # get delegated
+    dp.log("Downloading stat file for RIR %s..." % (rir))
+    dlg_tmpfile = commons.utils.get_tmp_file_name("delegated-lacnic-latest")
+    commons.getfile.getfile("ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest", dlg_tmpfile, 43200)
+    dp.log(" OK\n")
+    
+    dp.log("Importing delegated stats in memory... ")
+    dlg_api = delegated.api.Delegated(dlg_tmpfile)
+    dlg_api.read_delegated()
+    dp.log(" OK\n")        
+    
+    dp.log("Dowloading validator output... ")
+    dlg_tmpfile = commons.utils.get_tmp_file_name("lacnic-roa-prefixes.csv")
+    commons.getfile.getfile("http://ripeval.labs.lacnic.net/rpki/batch-validation/latest/lacnic.tal-roa-prefixes.csv", dlg_tmpfile, 3600)        
+    dp.log(" OK\n")
+    
+    dp.log("Importing RIPE validator output for RIR %s... " % (rir))
+    ripeval_stats = rpki.ripeval_stats.BatchValidationResults()
+    ripeval_stats.read_csv(dlg_tmpfile)
+    dp.log(" OK\n")
+    
+    ## process ROAs
+    rx = ripeval_stats.query("1=1", {})
+    for row in rx:
+        sk.incKey('ripeval-proc-rows')
+        pfx1 = ipaddr.IPNetwork(row['prefix'])
+        pfx2 = ipaddr.IPNetwork(pfx1.network)
+        drec = dlg_api.resource_find_inside(str(pfx2))
+        if drec:
+            if roa_query_in:
+                if roa_query_in.roa_test(drec, row):
+                    sk.incKey('matched-rows')
+                    roa_query_in.roa_match(drec, row)
                 else:
                     sk.incKey('non-matched-rows')
-                    pass
+                    roa_query_in.roa_no_match(drec, row)
+            elif query:
+                if eval(query, None, drec):
+                    print "prefix %s, alloc_pfx %s, origin_as %s, cc %s" % (row['prefix'], drec['prefix'], row['origin_as'], drec['cc'])
+                    sk.incKey('matched-rows')
             else:
-                sk.incKey('rows-not-found-in-dlg')
-                dp.log("\n ERROR: prefix :%s: should have been found in dlg file\n" % (row['prefix']))
-        ## end for
-        roa_query_in.finalize()
-        
-        print "\n\nRun Stats:\n"
-        print sk
-        
-        print "*** END Run\n"
-        return 0
+                sk.incKey('non-matched-rows')
+                pass
+        else:
+            sk.incKey('rows-not-found-in-dlg')
+            dp.log("\n ERROR: prefix :%s: should have been found in dlg file\n" % (row['prefix']))
+    ## end for
+    roa_query_in.finalize()
     
-    except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
-        return 0
-    except Exception as e:
-        if DEBUG or TESTRUN:
-            print e.message
-            raise e
-        indent = len(program_name) * " "
-        sys.stderr.write(program_name + ": " + repr(e) + "\n")
-        sys.stderr.write(indent + "  for help use --help\n")
-        return 2
+    print "\n\nRun Stats:\n"
+    print sk
+    
+    print "*** END Run\n"
+    return 0    
+#------------------------------------------------------------------------------------
+
+def main(argv=None): # IGNORE:C0111
+    '''Command line options.'''
+    
+    stats_runner(argv)
+
+    #===========================================================================
+    # try:
+    #     stats_runner(argv)
+    # except KeyboardInterrupt:
+    #     ## handle keyboard interrupt ###
+    #     return 0
+    # except Exception as e:
+    #     if DEBUG or TESTRUN:
+    #         print e.message
+    #         raise e
+    #     indent = len(program_name) * " "
+    #     sys.stderr.write(program_name + ": " + repr(e) + "\n")
+    #     sys.stderr.write(indent + "  for help use --help\n")
+    #     return 2
+    #===========================================================================
 
 if __name__ == "__main__":
     if DEBUG:
--- bin/rq_roas_per_cc.py
+++ bin/rq_roas_per_cc.py
@@ -16,21 +16,25 @@ class RoaQuery():
 		self.totals4 = {}
 		self.totals6 = {}
 
-	def roa_test(self, drec, dlg_row):
-		
-		if dlg_row['type'] == 'ipv4':
-				self.totals4 = self.totals4.get(drec['cc'], 0) + int ( dlg_row['value'] / 256 )
-		elif dlg_row['type'] == 'ipv6':
-				n48s = 0
-				self.totals6 = self.totals6.get(drec['cc'], 0) + n48s
-		
-		if drec['cc'] in self.ccs:
+	def roa_test(self, dlg_row, drec):
+				
+		if dlg_row['cc'] in self.ccs:
 			return True
 		else:
 			return False
 
-	def roa_match(self, drec, dlg_row):
-		self.counts[drec['cc']] = self.counts.get(drec['cc'], 0) + 1
+	def roa_match(self, dlg_row, drec):
+		try:
+			if dlg_row['type'] == 'ipv4':
+					self.totals4[dlg_row['cc']] = self.totals4.get(dlg_row['cc'], 0) + int ( int(dlg_row['value']) / 256 )
+			elif dlg_row['type'] == 'ipv6':
+					n48s = pow(2, 32-int(dlg_row['value'])) 
+					self.totals6[dlg_row['cc']] = self.totals6.get(dlg_row['cc'], 0) + n48s
+		except IndexError:
+			print "Invalid lines:\n dlg: %s\n drec: %s\n" % (dict(dlg_row), dict(drec))
+			raise
+		
+		self.counts[dlg_row['cc']] = self.counts.get(dlg_row['cc'], 0) + 1
 
 	def roa_no_match(self, drec, dlg_row):
 		print "non-matched row cc: %s, pfx %s" % (drec['cc'], drec['prefix'])
@@ -38,4 +42,6 @@ class RoaQuery():
 	def finalize(self, extra=None):
 		print "\n"
 		for x in self.ccs:
-			print "prefixes included in roas for country code %s: %s" % (x, self.counts.get(x,0))
+			print "prefixes included in roas for country code %s: %s, equivalent to %s /24s and %s /32s" % (
+				x, self.counts.get(x,0), self.totals4.get(x, 0), self.totals6.get(x,0) 
+			)
