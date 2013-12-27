c7a94f60ee9661c9936d2f3ca6d5ccb5364dd35c
Carlos M. Martinez <m@xt6.us>
2013-12-26 23:29:26 -0200

    added properties for servers ripeval

--- etc/properties.py
+++ etc/properties.py
@@ -5,7 +5,15 @@
 
 import os
 
+## System paths
 srchome = os.environ.get('SRCHOME', '')
 
 paths = { 'tmp': "%s/tmp" % (srchome),
           'etc': "%s/etc" % (srchome) }
+
+## -------------
+
+## Servers
+servers = { 'ripeval': {'http://ripeval.labs.lacnic.net:8080'}
+           }
+## -------------
