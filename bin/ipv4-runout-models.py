ecfb4d5df715911f470ae33f06e16f0704aae5bc
Carlos M. Martinez <carlos@xt6.us>
2014-03-06 01:02:25 +0000

    commit

--- bin/ipv4-runout-models.py
+++ bin/ipv4-runout-models.py
@@ -2,9 +2,14 @@
 # ipv4 runout models
 # (c) carlos@lacnic.net 2014-03-05
 ##################################################################
-import numpy
+from numpy import *
 
 # fetch data
+print "Fetching IPv4... ",
+time_series = array([1,2,3,4,5,6,7,8,9,10])
+freeipv4_series = array([1000,900,850,820,750,720,730,700,650,640])
+print "done!"
+
 
 # load into numpy arrays
 
